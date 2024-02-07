00a356a710ae8061f7f5d023e51c63c34ad34cb0
Mattia Mancini <m.c.mancini@exeter.ac.uk>
2024-02-07 14:48:28 +0000

    Drafted template for the class "Farmer"

---
+++ farm_uncertainty/farmers.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 LEEP - University of Exeter (UK)
+# Mattia C. Mancini (m.c.mancini@exeter.ac.uk), 
+# Amy Binner (a.r.binner@exeter.ac.uk)
+# February 2024
+"""
+Class Farmer to generate instances of farmers which constitute the
+main agents of the agent-based model simulating farmer uptake of
+agri-environmental schemes on their land based on uncertain future
+conditions (prices and climate)
+"""
+
+class Farmer():
+    """
+    Farmer
+    ------------------------
+
+    Required input parameters for initialisation:
+    :param famer_id: unique identifier of a farmer. Needed to keep track
+           of farmer behaviour
+    
+    Methods defined here:
+
+    __str__(self, /)
+        Return str(self)
+    
+    """
+
+    def __init__(self, ):
+        pass
+    
